597143b06d2b6af08e702d00466a31b707a6c407
Chirag Singh Manral <chiragmanral2003@gmail.com>
2025-04-10 17:25:52 +0530

    new code

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -27,6 +27,38 @@ export class TasksService {
           summary:
             'Prepare and describe an issue template which will help with project management',
           dueDate: '2024-06-15',
+        },
+        {
+          id: 't4',
+          userId: 'u2',
+          title: 'Master Angular',
+          summary:
+            'Learn all the basic and advanced features of Angular & how to apply them.',
+          dueDate: '2025-12-31',
+        },
+        {
+          id: 't5',
+          userId: 'u4',
+          title: 'Master Angular',
+          summary:
+            'Learn all the basic and advanced features of Angular & how to apply them.',
+          dueDate: '2025-12-31',
+        },
+        {
+          id: 't6',
+          userId: 'u5',
+          title: 'Master Angular',
+          summary:
+            'Learn all the basic and advanced features of Angular & how to apply them.',
+          dueDate: '2025-12-31',
+        },
+        {
+          id: 't7',
+          userId: 'u6',
+          title: 'Master Angular',
+          summary:
+            'Learn all the basic and advanced features of Angular & how to apply them.',
+          dueDate: '2025-12-31',
         }
     ];
 
